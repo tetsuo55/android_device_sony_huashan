c26bff349cf13193ab93eddc24af3668d2d055ef
AdrianDC <radian.dc@gmail.com>
2016-06-07 11:14:17 +0200

    huashan: Update the device tree to match CM-13.0 tree improvements
    Change-Id: I00142fda335fe14b6666fcb82b8579bfaa13d3ca
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

--- omni_huashan.mk
+++ omni_huashan.mk
@@ -1,16 +1,38 @@
-# Device configuration
-$(call inherit-product, device/sony/huashan/full_huashan.mk)
+#
+# Copyright (C) 2011 The Android Open Source Project
+# Copyright (C) 2015-2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit device configurations
+$(call inherit-product, device/sony/huashan/device.mk)
+
+# Device display
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Device identification
+PRODUCT_DEVICE := huashan
+PRODUCT_NAME := omni_huashan
+PRODUCT_RELEASE_NAME := huashan
+PRODUCT_BRAND := Sony
+PRODUCT_MANUFACTURER := Sony
+PRODUCT_MODEL := Xperia SP
 
 # Prebuilts
 PRODUCT_COPY_FILES += \
     device/sony/huashan/prebuilts/kernel:kernel
 
-# Release name
-PRODUCT_RELEASE_NAME := huashan
-
 # Build fingerprints
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C5303 BUILD_FINGERPRINT=Sony/C5303/C5303:4.3/12.1.A.1.207/Nvt_nw:user/release-keys PRIVATE_BUILD_DESC="C5303-user 4.3 JB-MR2-VISKAN-140318-1014 227 test-keys"
-
-# Device identification
-PRODUCT_NAME := omni_huashan
-PRODUCT_DEVICE := huashan
